0ce43573e8177446ceb86fe7d8854180b6f170c6
Zakha123-cyber <aguscow6@gmail.com>
2024-05-23 18:27:38 +0700

    coba coba

---
+++ Project PBO/App/Models/Role.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_PBO.App.Models
+{
+    public class Role
+    {
+        public string nama { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string alamat { get; set; }
+        public string kota { get; set; }
+        public string negara { get; set; }
+        public string no_telfon { get; set; }
+        public string id_jenis_profil { get; set; }
+    }
+}
