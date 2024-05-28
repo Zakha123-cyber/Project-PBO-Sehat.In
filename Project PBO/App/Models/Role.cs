fcde9b54c30330fe69f52d801c1f68446e8bea5c
deviesAde <deviesade@gmail.com>
2024-05-28 22:45:16 +0700

    CEK KONEKSINYA TULOOONG

--- Project PBO/App/Models/Role.cs
+++ Project PBO/App/Models/Role.cs
@@ -8,13 +8,5 @@ namespace Project_PBO.App.Models
 {
     public class Role
     {
-        public string nama { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string alamat { get; set; }
-        public string kota { get; set; }
-        public string negara { get; set; }
-        public string no_telfon { get; set; }
-        public string id_jenis_profil { get; set; }
-    }
+   
 }
